4bc45c11aa65ef4cc3ef3d77016f6bab628bc699
vuhoangminh <minhmanutd@gmail.com>
2018-12-02 18:35:30 +0100

    no more

--- dataset/test_normalize_minh.py
+++ dataset/test_normalize_minh.py
@@ -4,8 +4,17 @@ import shutil
 import sys
 import nibabel as nib
 
+volume_path = "C:/Users/minhm/Documents/GitHub/3DUnetCNN_BRATS/brats/data_train/denoised_preprocessed/HGG/Brats18_2013_2_1/t1.nii.gz"
+truth_path = "C:/Users/minhm/Documents/GitHub/3DUnetCNN_BRATS/brats/data_train/denoised_preprocessed/HGG/Brats18_2013_2_1/truth.nii.gz"
+volume = nib.load(volume_path)
+volume = volume.get_fdata()
+truth = nib.load(truth_path)
+truth = truth.get_fdata()
+
+
 def main():
     print("hello")
+
 
 if __name__ == '__main__':
     main()
