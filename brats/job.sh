2a4b918386e87542c231fe5fba15ba7e8923291f
Minh Vu <minhmanutd@gmail.com>
2018-12-23 14:09:42 +0100

    job

--- brats/job.sh
+++ brats/job.sh
@@ -2,7 +2,7 @@
 #SBATCH -A SNIC2018-3-406
 #SBATCH -n 1
 ##SBATCH --exclusive
-#SBATCH --time=04-00:00:00
+#SBATCH --time=05-00:00:00
 #SBATCH --error=%J_error.out
 #SBATCH --output=%J_output.out
 #SBATCH --gres=gpu:k80:1
@@ -11,7 +11,7 @@ ml GCC/6.4.0-2.28  CUDA/9.0.176  OpenMPI/2.1.1
 
 export KERAS_BACKEND="tensorflow"
 
-export command="python brats/train.py -t "0" -o "0" -n "01" -de "median" -hi "0" -ba 2"
+export command="python brats/train.py -t "0" -o "0" -n "01" -de "0" -hi "0" -m "unet" -ba 2"
 echo "$command"
 srun $command
 
