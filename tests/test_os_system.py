c73c1a42a56d0d5a0c495893e1c415dd7f0c1f56
Minh Vu <minhmanutd@gmail.com>
2019-01-04 22:23:27 +0100

    [fix finetune]

---
+++ tests/test_os_system.py
@@ -0,0 +1,9 @@
+import os
+
+cmd = "python brats/train25d.py -t \"0\" -o \"0\" -n \"z\" -de \"bm4d\" -hi \"0\" -m \"isensee\" -l \"minh\" -ba 64"
+
+os.system(cmd)
+
+
+
+# python brats/train25d.py -t "0" -o "0" -n "01" -de "0" -hi "0" -m "seisensee" -ba 32 -l "minh"
