3158f9be6ef8d1be7f01812e3c4c81d73faf4a88
vuhoangminh <minhmanutd@gmail.com>
2019-06-04 21:51:14 +0200

    []

--- projects/pros/generator.py
+++ projects/pros/generator.py
@@ -144,10 +144,10 @@ def get_train_valid_test_split(data_file, training_file, validation_file,
         nb_samples = data_file.root.data.shape[0]
         sample_list = list(range(nb_samples))
         training_list, testing_list = split_list(
-            sample_list, split=0.5)
+            sample_list, split=data_split)
 
         training_list, validation_list = split_list(
-            training_list, split=0.8)
+            training_list, split=data_split)
 
         pickle_dump(training_list, training_file)
         pickle_dump(validation_list, validation_file)
